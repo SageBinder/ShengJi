759c485321b536cc1206ad35af243f958b3f9822
SageBinder <sagebin13@gmail.com>
2018-08-02 01:30:33 -0700

    Initial default LibGDX template commit

---
+++ desktop/src/com/sage/shengji/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sage.shengji.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sage.shengji.ShengJi;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShengJi(), config);
+	}
+}
